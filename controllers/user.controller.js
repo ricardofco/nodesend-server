d96bbba54272690165f46dd6027fc4075de49fdb
Ricardo Francisco <ricardofco.35@gmail.com>
2020-07-28 18:41:36 -0500

    [Users] hashing passwords & model validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,26 @@
+const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 const User = require('../models/User');
 
 exports.addUser = async (req, res) => {
-  const { email } = req.body;
+  const validationErrors = validationResult(req);
+  if (!validationErrors.isEmpty()) {
+    return res.status(400).json({ error: validationErrors.array() });
+  }
+  const { email, password } = req.body;
   let user = await User.findOne({ email });
   if (user) {
     return res.status(400).json({ msg: 'El usuario ya está registrado' });
   }
   user = new User(req.body);
-  user.save();
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(password, salt);
 
-  return res.json({ msg: 'Usuario creado correctamente' });
+  try {
+    user.save();
+    return res.json({ msg: 'Usuario creado correctamente' });
+  } catch (error) {
+    console.log(error);
+    return { error };
+  }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const { check } = require('express-validator');
 const usuarioController = require('../controllers/user.controller');
 
 const router = express.Router();
 
-router.post('/', usuarioController.addUser);
+router.post(
+  '/',
+  [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El formato de correo no es valido').isEmail(),
+    check('password', 'La contraseña debe contener al menos 6 caracteres').isLength({ min: 6 }),
+  ],
+  usuarioController.addUser
+);
 
 module.exports = router;
