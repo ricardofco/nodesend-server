cd7cba31cca616d4e0b7e5455eb5cbbec66a8489
Ricardo Francisco <ricardofco.35@gmail.com>
2020-07-30 18:05:28 -0500

    Adding more fields for authenticated users

--- controllers/link.controller.js
+++ controllers/link.controller.js
@@ -1,13 +1,25 @@
 const shortid = require('shortid');
+const bcrypt = require('bcrypt');
 const Link = require('../models/Link');
 
 exports.addLink = async (req, res) => {
-  const { originalName, password } = req.body;
+  const { originalName } = req.body;
   const link = new Link();
   link.url = shortid.generate();
   link.name = shortid.generate();
   link.originalName = originalName;
-  link.password = password;
+
+  if (req.user) {
+    const { downloads, password } = req.body;
+    if (downloads) {
+      link.downloads = downloads;
+    }
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      link.password = await bcrypt.hash(password, salt);
+    }
+    link.author = req.user.id;
+  }
 
   try {
     await link.save();
